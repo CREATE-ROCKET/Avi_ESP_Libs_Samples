4e822dd3824cc58dcb77c0339b3f1f6ba2e83622
KAMEWAKA <r.kamewaka@gmail.com>
2022-10-18 18:47:18 +0900

    ピトー管IC用ライブラリ(計算機能有り)

--- PITOIC/src/PITOIC.h
+++ PITOIC/src/PITOIC.h
@@ -9,20 +9,24 @@
 class PITOIC
 {
     int CS;
+    float constantVal;
     int deviceHandle{-1};
     SPICREATE::SPICreate *PITOICSPI;
 
 public:
-    void begin(SPICREATE::SPICreate *targetSPI, int cs, uint32_t freq = 8000000);
-    void Get(uint8_t *rx);
+    uint16_t PitoRawData;
+    float WindSpeed;
+    void begin(SPICREATE::SPICreate *targetSPI, int cs, uint32_t freq = 8000000, float constant = 1.);
+    void Get(uint8_t *rx, int PlessureVal = 101325, float TemperatureVal = 14.5); // Oshima Nov. Average Temp.
     // *** LITTLE ENDIAN ***
     // (uint32_t)rx[0] << 8 | (uint32_t)rx[1] means PITO raw data
 };
 
-void PITOIC::begin(SPICREATE::SPICreate *targetSPI, int cs, uint32_t freq)
+void PITOIC::begin(SPICREATE::SPICreate *targetSPI, int cs, uint32_t freq, float alpha)
 {
     CS = cs;
     PITOICSPI = targetSPI;
+    constantVal = alpha;
     spi_device_interface_config_t if_cfg = {};
 
     // if_cfg.spics_io_num = cs;
@@ -43,7 +47,7 @@ void PITOIC::begin(SPICREATE::SPICreate *targetSPI, int cs, uint32_t freq)
     return;
 }
 
-void PITOIC::Get(uint8_t *rx)
+void PITOIC::Get(uint8_t *rx, int PlessureVal, float TemperatureVal)
 {
     spi_transaction_t comm = {};
     comm.flags = SPI_TRANS_VARIABLE_CMD | SPI_TRANS_VARIABLE_ADDR;
@@ -59,6 +63,16 @@ void PITOIC::Get(uint8_t *rx)
     spi_transaction.command_bits = 0;
 
     PITOICSPI->transmit(NULL, rx, 2, deviceHandle);
+
+    PitoRawData = rx[0] << 8 | rx[1];
+    float AirDensity = PlessureVal / ((TemperatureVal + 273.5) * 2.87);
+    double Speed_Calc_Buf = (PitoRawData / 16384 * 100 - 10) * 250 - 10000;
+    if (Speed_Calc_Buf < 0)
+    {
+        Speed_Calc_Buf = Speed_Calc_Buf * -1;
+    }
+    Speed_Calc_Buf = sqrt(Speed_Calc_Buf * 2 / AirDensity);
+    WindSpeed = (float)Speed_Calc_Buf * constantVal;
     return;
 }
 
